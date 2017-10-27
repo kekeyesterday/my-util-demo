c1d35375c9d84658d76f09788176b618486e1aa1
javakill@163.com <javakill@163.com>
2017-10-27 17:25:47 +0800

    git测试
    Change-Id: I9e769a200f7a492fa956cdce4622e4da38157e2f

---
+++ src/main/java/com/jimmy/common/util/DateConvert.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2007-2013 SAIC. All Rights Reserved.
+ * This software is published under the terms of the SAIC IT Dept.
+ *
+ * @Project: Telematics
+ */
+package com.jimmy.common.util;
+
+/**
+ * 客户端 User: jozbt Date: 12-12-5 Time: 上午10:56 To change this template use File
+ * | Settings | File Templates.
+ */
+public abstract class DateConvert {
+	
+	
+}
